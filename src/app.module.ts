90675d20bc929a92407c76528b84cd25dfc6a1fa
melovitor <vitormelobatista@hotmail.com>
2023-10-04 15:24:06 -0300

    WIP - Refatorando configs.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { UserModule } from './Users/user.module';
 import { ProductModule } from './Products/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfigService } from './Config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
     ProductModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
       useClass: PostgresConfigService,
       inject: [PostgresConfigService],
